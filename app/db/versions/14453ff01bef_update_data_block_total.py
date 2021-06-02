aba19211624ac9af4e3124e7a8f2aa7e5e1cd849
pgolovkin <golovkin@soramitsu.co.jp>
2021-06-03 00:15:02 +0300

    PSS-938 fix sql

--- app/db/versions/14453ff01bef_update_data_block_total.py
+++ app/db/versions/14453ff01bef_update_data_block_total.py
@@ -20,27 +20,30 @@ depends_on = None
 
 
 def upgrade():
-    op.add_column('data_block_total', sa.Column('total_bridge_income', sa.Numeric(precision=65, scale=0)))
-    op.add_column('data_block_total', sa.Column('total_bridge_outcome', sa.Numeric(precision=65, scale=0)))
-    op.add_column('data_block', sa.Column('count_bridge_income', sa.Integer(), nullable=False))
-    op.add_column('data_block', sa.Column('count_bridge_outcome', sa.Integer(), nullable=False))
-    op.add_column('data_reorg_block', sa.Column('count_bridge_income', sa.Integer(), nullable=False))
-    op.add_column('data_reorg_block', sa.Column('count_bridge_outcome', sa.Integer(), nullable=False))
-    sql_outcome = "update data_block_total set total_bridge_outcome = " \
-                    "(select count(*) " \
-                    "from data_extrinsic " \
-                    "where data_extrinsic.module_id = 'EthBridge' " \
-                        "and data_extrinsic.call_id = 'transfer_to_sidechain') " \
-                  "order by id desc limit 1"
-    op.execute(sql_outcome)
+    try:
+        op.add_column('data_block_total', sa.Column('total_bridge_income', sa.Numeric(precision=65, scale=0)))
+        op.add_column('data_block_total', sa.Column('total_bridge_outcome', sa.Numeric(precision=65, scale=0)))
+        op.add_column('data_block', sa.Column('count_bridge_income', sa.Integer(), nullable=False))
+        op.add_column('data_block', sa.Column('count_bridge_outcome', sa.Integer(), nullable=False))
+        op.add_column('data_reorg_block', sa.Column('count_bridge_income', sa.Integer(), nullable=False))
+        op.add_column('data_reorg_block', sa.Column('count_bridge_outcome', sa.Integer(), nullable=False))
+        sql_outcome = "update data_block_total set total_bridge_outcome = " \
+                        "(select count(*) " \
+                        "from data_extrinsic " \
+                        "where data_extrinsic.module_id = 'EthBridge' " \
+                            "and data_extrinsic.call_id = 'transfer_to_sidechain') " \
+                      "order by id desc limit 1"
+        op.execute(sql_outcome)
 
-    sql_income = "update data_block_total set total_bridge_income = " \
-                 "(select count(*) " \
-                    "from data_event " \
-                    "where data_event.module_id = 'ethbridge' " \
-                        "and data_event.event_id = 'IncomingRequestFinalized') " \
-                "order by id desc limit 1 "
-    op.execute(sql_income)
+        sql_income = "update data_block_total set total_bridge_income = " \
+                     "(select count(*) " \
+                        "from data_event " \
+                        "where data_event.module_id = 'ethbridge' " \
+                            "and data_event.event_id = 'IncomingRequestFinalized') " \
+                    "order by id desc limit 1 "
+        op.execute(sql_income)
+    except:
+        print("Error running migration, but we can proceed")
 
 
 def downgrade():
